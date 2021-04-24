256cbafffb8426132de63608dba1075327085278
sling1776 <spencerlingwall@gmail.com>
2021-04-24 17:05:53 -0600

    modified to implement table first before getting the strings from the table.

--- src/main.java
+++ src/main.java
@@ -42,12 +42,16 @@ public class main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("set2.txt");
+        File file = new File("set1.txt");
         Scanner scanner = new Scanner(file);
         int i = 0;
         while(scanner.hasNext()){
             prices[i] = scanner.nextInt();
             i++;
+        }
+
+        for(int j = 1; j< MAX_TABLE_SIZE ; j++){
+            getTableEntry(j, MAX_GROUP_SIZE);
         }
 
         for(int j = 1; j< MAX_TABLE_SIZE ; j++){
